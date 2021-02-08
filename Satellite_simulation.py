f08a37bc27db7a151fc6097f652db0a53a194e03
Ulrich <louwuj@gmail.com>
2021-02-08 17:32:20 +0200

    Disturbance functions

--- Satellite_simulation.py
+++ Satellite_simulation.py
@@ -21,23 +21,36 @@ def quaternion_error(current_quaternion, command_quaternion):
 
 
 class SET_PARAMS:
-    eccentricicity = 0.0002704
-    inclination = 97.2927 #degrees
+    eccentricicity = 0.000092
+    inclination = 97.4 #degrees
     Semi_major_axis = 6879.55 #km The distance from the satellite to the earth + the earth radius
-    RAAN = 0 #Right ascension of the ascending node
+    RAAN = 275 #Right ascension of the ascending node in degrees
     AP = 0 #argument of perigee
     Re = 6371.2 #km magnetic reference radius
-    Mean_motion = 15.215 #rev/day
+    Mean_motion = 15.2355 #rev/day
+    Mean_anomaly = 29.3 #degrees
+    Argument_of_perigee = 57.4 #in degrees
+    Drag_term = 0.000194
+    omega = Argument_of_perigee
+    Mass = 20 #kg
+    Dimensions = np.array(([0.3, 0.3, 0.4])) # Lx, Ly, Lz
     Period = 5678.7 #seconds
-    Ix = 0.0071 #kg.m^2
-    Iy = 0.035 #kg.m^2
-    Iz = 0.337 #kg.m^2
+    Ix = 0.4 #kg.m^2
+    Iy = 0.45 #kg.m^2
+    Iz = 0.3 #kg.m^2
+    Iw = 88.1e-6 #kgm^2 Inertia of the RW-06 wheel
     wo = (Mean_motion/(3600*24)) / (2*pi*Semi_major_axis * 1000) #rad/s
     k = 2 #order of expansion
     g = -2.2 #defined by IGRF
     h = -8 #
     m = 10
     n = 10
+    J_t = 2500000
+    wbi = np.array([1],[1],[1])
+    theta_d_max = 2 #degrees per second (theta derived), angular velocity
+    theta_d_d = 0.133 # degrees per second^2 (rotation speed derived), angular acceleration
+    h_ws_max = 15.7 # mNms
+    N_ws_max = 1.05 #mNm
 
 
 class Sensors:
@@ -57,7 +70,7 @@ class Actuators:
     def magnetic_torgue_rods(self):
         self.MTR_num = 3
 
-    def nano_reaction_wheels(self):
+    def nano_reaction_wheels(self):     #RW-0.06 Sinclair Interplanetary
         self.RW_num = 3
 
 class Satelite_model:
@@ -93,8 +106,8 @@ class Satelite_model:
         q_derived = 0.5 * np.matmul(Q, Aw)
         return q_derived
 
-    def wbo_func(self, wbi, A):
-        wbo = wbi - np.matmul(A, np.array([0],[-SET_PARAMS.wo],[0]))
+    def wbo_func(self, A):
+        wbo = SET_PARAMS.wbi - np.matmul(A, np.array([0],[-SET_PARAMS.wo],[0]))
         return wbo
 
     def scalar_potential_function(self, orbit_radius, coevelation, longitude):
@@ -127,8 +140,69 @@ class Satelite_model:
         return delta_V
 
     def Sun_Position(self):
-        
+        T_jc = (SET_PARAMS.J_t - 2452545)/36525
+        M_o = 357.527723300 + 35999.050340*T_jc     #in degrees
+        lambda_Mo = 280.460618400 + 36000.770053610*T_jc        #degrees
+        lambda_e = lambda_Mo + 1.914666471*np.sin(M_o*pi/180) + 0.019994643*math.sin(2*M_o*pi/180)      #degrees
+        epsilon =  23.439291 - 0.013004200*T_jc                 #degrees
+        r_o = 1.00140612 - 0.016708617*np.cos(M_o*pi/180) - 0.000139589*np.cos(2*M_o*pi/180)        #degrees
+        rsun = r_o * np.array(([np.cos(lambda_e*pi/180)],[np.cos(epsilon*pi/180)*np.sin(lambda_e*pi/180)],[np.sin(epsilon*pi/180)*np.sin(lambda_e*pi/180)]))
+        return rsun*(149597871)     #in km
 
+class Disturbances:
+    def __init__(self):
+        pass
+
+    def Gravity_gradient_func(self, AOB):
+        zoB = AOB * np.array(([0],[0],[1]))
+        kgx = 3 * SET_PARAMS.wo**2 * (SET_PARAMS.Iz - SET_PARAMS.Iy)
+        kgy = 3 * SET_PARAMS.wo**2 * (SET_PARAMS.Ix - SET_PARAMS.Iz)
+        kgz = 3 * SET_PARAMS.wo**2 * (SET_PARAMS.Iy - SET_PARAMS.Ix)
+        Ngg = np.array(([kgx*AOB[1][2]*AOB[2][2]],[kgy*AOB[0][2]*AOB[2][2]],[kgz*AOB[0][2]*AOB[1][2]]))
+        return Ngg
+
+    def Aerodynamic(self, v_ab, Surface_area_i, incidence_angle,
+                        normal_accommodation, ratio_of_molecular_exit, offset_vector, 
+                        unit_inward_normal, h, h_o, H):
+        Ai = Surface_area_i
+        alpha_i = incidence_angle
+        sigma_t = 0.8       # tangential_accommodation
+        sigma_n = 0.8  # normal_accommodation
+        S = 0.05        # ratio_of_molecular_exit
+        r_pi = offset_vector
+        n_i = unit_inward_normal
+        p = 0.5 * (p_o * np.exp(-(h-h_o)/H))
+        va = np.arraty(([0],[0],[w_e])) * r_sat - v_sat
+        N_aero = 0
+        for i in range(n):
+            N_aero = N_aero + p * np.linalg.norm(va)**2 * Ai * np.heaviside(np.cos(alpha_i))*np.cos(alpha_i)*sigma_t*(r_pi) + 
+                        (sigma_n * S + (2-sigma_n - sigma_t)*np.cos(alpha_i)*(r_pi))
+
+        return N_aero
+
+    def Wheel_Imbalance(self, rotation_rate):
+        # Static imbalance with one mass a distance r from the centre of the flywheel
+        # m - mass of imbalance in kg
+        # r - distance from centre of flywheel in m
+        Us = 0.0208 / 1e-5 #For the RW-0.06 wheels in kg/m; Us = m*r
+        omega = rotation_rate #rad/s rotation rate of wheel
+        phi_s = 0 #arbitrary phase
+        w_z = np.array(([0,0,0])) #np.array 
+
+        F_zs = Us * omega**2 * np.array(([np.sin(omega*t + phi_s)],[np.cos(omega*t + phi_s)], [0]))
+
+        N_zs = np.matmul(w_z, F_zs)
+
+        # Dynamic imbalance with two masses seperated by 180 degrees and distance, d
+        # d - width of flywheel in m
+        # r - distance from flywheel
+        # m - mass in kg
+        Ud = 0.0208 / 1e-7 #For the RW-0.06 wheels in kg/m^2; Ud = m*r*d
+        N_zd = Ud*omega**2 * np.array(([np.sin(omega*t + phi_s)],[np.cos(omega*t + phi_s)], [0]))
+
+        return N_zs
+
+        
 class Simulation:
     def __init__(self):
         print('Start Simulation')
@@ -167,4 +241,4 @@ if __name__ == "__main__":
     current_quaternion = euler_to_quaternion(0.2, 0.3, 0.2)
     AOB = sat.AOB_func(current_quaternion)
     print(sat.scalar_potential_function(SET_PARAMS.Semi_major_axis, SET_PARAMS.inclination-90, 1))
- 
+    print(Disturbances().Gravity_gradient_func())
